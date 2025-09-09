35e591ab4bf3431d4cd19dac1b87ba4debf184ef
edgarcltop <edgarclcace@gmail.com>
2025-09-09 20:12:41 +0200

    chore: prevent flickering if user is already logged in

--- apps/native/app/(root)/_layout.tsx
+++ apps/native/app/(root)/_layout.tsx
@@ -1,5 +1,6 @@
 import { useConvexAuth } from "convex/react";
 import { Stack } from "expo-router";
+import { Spinner } from "heroui-native";
 import { useNavigationOptions } from "@/hooks/useNavigationOptions";
 
 export const unstable_settings = {
@@ -11,23 +12,31 @@ export default function RootLayout() {
   const { root } = useNavigationOptions();
   /* --------------------------------- return --------------------------------- */
   return (
-    <Stack>
-      {/* AUTH STACK */}
-      <Stack.Protected guard={!isAuthenticated}>
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      </Stack.Protected>
-      {/* AUTHENTICATED NESTED STACK */}
-      <Stack.Protected guard={isAuthenticated}>
-        {/* MAIN STACK*/}
-        <Stack.Screen
-          name="(main)"
-          options={{
-            title: "",
-            headerShown: false,
-            ...root,
-          }}
-        />
-      </Stack.Protected>
-    </Stack>
+    <>
+      {isLoading ? (
+        <View className="flex-1 items-center justify-center">
+          <Spinner size="lg" />
+        </View>
+      ) : (
+        <Stack>
+          {/* AUTH STACK */}
+          <Stack.Protected guard={!isAuthenticated}>
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          </Stack.Protected>
+          {/* AUTHENTICATED NESTED STACK */}
+          <Stack.Protected guard={isAuthenticated}>
+            {/* MAIN STACK*/}
+            <Stack.Screen
+              name="(main)"
+              options={{
+                title: "",
+                headerShown: false,
+                ...root,
+              }}
+            />
+          </Stack.Protected>
+        </Stack>
+      )}
+    </>
   );
 }
